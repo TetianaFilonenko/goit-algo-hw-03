b5beee6f79f96ebc4525559a219e70e586718b62
Tania Filonenko <tetiana.filonenko2012@gmail.com>
2024-05-05 13:32:04 +0100

    Update logic to only have root dest for files

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import sys
 
+
 def copy_files(src, dest):
     try:
         os.makedirs(dest, exist_ok=True)
@@ -11,17 +12,18 @@ def copy_files(src, dest):
             full_file_name = os.path.join(src, file_name)
 
             if os.path.isfile(full_file_name):
-                extension = os.path.splitext(file_name)[1].lstrip('.').lower()
+                extension = os.path.splitext(file_name)[1].lstrip(".").lower()
                 if not extension:
-                    extension = 'no_extension'
+                    extension = "no_extension"
                 dest_subdir = os.path.join(dest, extension)
                 os.makedirs(dest_subdir, exist_ok=True)
 
                 shutil.copy(full_file_name, dest_subdir)
             elif os.path.isdir(full_file_name):
-                copy_files(full_file_name, os.path.join(dest, file_name))
+                copy_files(full_file_name, dest)
     except Exception as e:
         print(f"Error: {e}")
+
 
 def main():
     if len(sys.argv) < 2:
@@ -32,9 +34,10 @@ def main():
     if len(sys.argv) >= 3:
         dest_directory = sys.argv[2]
     else:
-        dest_directory = 'dest'
+        dest_directory = "dest"
 
     copy_files(src_directory, dest_directory)
+
 
 if __name__ == "__main__":
     main()
